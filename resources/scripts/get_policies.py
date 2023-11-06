8501f97caf1c494db468873bb392c5230875e7d0
David Solow <davesolow@gmail.com>
2023-11-05 19:25:37 -0500

    get_policies makes a basic retrieval on the congress.gov api. It works locally but not on the Airflow DAG - fixed this next.

--- resources/scripts/get_policies.py
+++ resources/scripts/get_policies.py
@@ -1,16 +1,30 @@
 import os
+from dotenv import load_dotenv
 import requests
-from bs4 import BeautifulSoup
 
 def get_policies(**context):
-    new_policies = [
-        {
-            'policy_id': '1234',
-            'policy_name': 'New Bill',
-            'policy_branch': 'Legislative',
-            'policy_type': 'House Bill',
-            'policy_url': 'www.newbill.com',
-            'policy_text': 'This is a sweet new bill'
-        }
-    ]
-    return new_policies
+    load_dotenv()
+    base_url = f'https://api.congress.gov/v3/bill'
+    query_params = {
+        'api_key': os.getenv('CONGRESSAPI'),
+        'format': 'json',
+        'limit': 1,
+        'offset': 0
+    }
+
+    response = requests.get(base_url, params=query_params)
+    if response.status_code == 200:
+        data = response.json()
+        new_policies = []
+        for bill in data['bills']:
+            new_policies.append({'policy_id': bill['number'],
+                                'policy_name': bill['title'],
+                                'policy_branch': bill['originChamberCode'],
+                                'policy_type': bill['type'],
+                                'policy_url': bill['url'],
+                                'policy_text': 'Placeholder text.'}
+            )
+        return new_policies
+    else:
+        print(f"Failed to fetch policies: {response.status_code}")
+        return []
